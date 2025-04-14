8332872ac769c56a8cb98a9e6d27bfe2a90dce68
winzepz <winzepz24@gmail.com>
2025-04-14 19:39:09 +0545

    Added session handling route and displayed session data

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -16,8 +16,24 @@ router.post("/register/step3", registerStep3);
 // Login route
 router.post("/login", loginUser);
 
-router.get('/meowmeow', (req, res) => {
-    res.json({ message: 'Meow' });
-  });
+router.get("/session", (req, res) => {
+  // Check if any of the session step data exists
+  if (req.session.step1Data || req.session.step2Data || req.session.step3Data) {
+    return res.json({ 
+      message: "Session data exists", 
+      step1Data: req.session.step1Data,
+      step2Data: req.session.step2Data,
+      step3Data: req.session.step3Data,
+    });
+  } else {
+    return res.status(404).json({ message: "Session data not found" }); // Session data not found
+  }
+});
+
+
+
+router.get("/meowmeow", (req, res) => {
+  res.json({ message: "Meow" });
+});
 
 module.exports = router;
