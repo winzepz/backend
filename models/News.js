bee24057b43e13a0d58f8a42b872575788a15095
winzepz <winzepz24@gmail.com>
2025-05-06 15:37:55 +0545

    fix news update time bug

--- models/News.js
+++ models/News.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 
 const newsSchema = new mongoose.Schema({
-  newsId: {type: String, required: true, unique: true },
+  newsId: { type: String, required: true, unique: true },
   title: { type: String, required: true },
   tags: [{ type: String }],
-  image: { type: String, required: true },   
-  content: { type: String, required: true },  
+  image: { type: String, required: true },
+  content: { type: String, required: true },
   authorId: { type: String, required: true },
   authorName: { type: String, required: true },
   status: {
@@ -14,9 +14,9 @@ const newsSchema = new mongoose.Schema({
     enum: ["pending", "published", "rejected"],
     default: "pending"
   },
-  isDraft: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
-  updatedAt: { type: Date, default: Date.now }
+  isDraft: { type: Boolean, default: false }
+}, {
+  timestamps: true
 });
 
 module.exports = mongoose.model("News", newsSchema);
